6986ca20e5bca81b545a09c8fc4b326b7057c6bb
Surya Darma <darmasurya2725@gmail.com>
2025-05-20 02:26:10 +0700

    Update

---
+++ app/src/main/java/com/example/brainquiz/KelasActivity.java
@@ -0,0 +1,12 @@
+package com.example.brainquiz;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KelasActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelas);
+    }
+}
